7c0be1670cb3ff68c9bc2a51dcfd9799a2c9331a
Antonio Ortega <I26510@verisk.com>
2022-05-05 13:13:11 -0600

    print env

--- ReviewFinishedAction/Program.cs
+++ ReviewFinishedAction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ReviewFinishedAction
 {
@@ -6,7 +7,16 @@ namespace ReviewFinishedAction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Environment variables:");
+
+            var variables = Environment.GetEnvironmentVariables();
+            if (variables != null)
+            {
+                foreach (DictionaryEntry de in variables)
+                {
+                    Console.WriteLine("  {0} = {1}", de.Key, de.Value);
+                }
+            }
         }
     }
 }
